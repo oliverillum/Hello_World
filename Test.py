63b700a4dbc1abe99045238172d9bc7484b8db51
oliverillum <123772297+oliverillum@users.noreply.github.com>
2023-02-06 08:40:22 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,17 +1,26 @@
-import os
+import pandas as pd
 
-def find_path(name, path):
-    for root, dirs, files in os.walk(path):
-        if name in dirs or name in files:
-            return root
-    return None
+# Load the Excel file
+df = pd.read_excel("testDataark.xlsx")
 
-root_dir = r'C:\Users\Oliver'
-target_name = 'Test345564.pdf'
-path = find_path(target_name, root_dir)
-if path:
-    print(f"Found at: {path}")
-else:
-    print(f"{target_name} not found")
+# Print the dataframe
+#print(df)
+
+# Get the email of a specific customer by their name
+def get_email(name):
+    email = df.loc[df['Customer name'] == name, 'VAT  nr. NO'].iloc[0]
+    return email
+
+# Get the customer number of a specific customer by their name
+def get_customer_nr(name):
+    customer_nr = df.loc[df['Customer name'] == name, 'Path'].iloc[0]
+    return customer_nr
+
+# Example usage
+email = get_email("7 Days") 
+#customer_nr = get_customer_nr("7 Days")
+print("Email:", email)
+#print("Customer Number:", customer_nr)
+
 
 
