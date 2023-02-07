e953b6a58664a9a8b20234a7c30977580d47e8b5
oliverillum <123772297+oliverillum@users.noreply.github.com>
2023-02-07 15:06:39 +0100

    Update MailSenderTest.py

--- MailSenderTest.py
+++ MailSenderTest.py
@@ -23,6 +23,8 @@ BODY = "Template2"
 #User = input("which user are running this script? ")
 period = input("what is the period 'yyyy MM'?" )
 customer = input("VAT nr. for the customer you want to send to?")
+df['VAT'] = df['VAT'].astype(int)
+customer = int(customer)
 
 pathToCustomerFolder = df.loc[df['VAT'] == customer, 'Folder'].values[0] 
 nameToCustomer = df.loc[df['VAT'] == customer, 'Name'].values[0]
@@ -39,17 +41,20 @@ target_name = (period +" Tolddeklarationsoversigt.pdf")
 path = find_path(target_name, root_dir)
 if path:
     print(f"Found at: {path}")
+    
 else:
     print(f"{target_name} not found")
-
+    logging.error(f"{target_name} not found")
 
 attachment = ((path) + "\\" + (target_name))
 
 
 
+
+
 # construct the email item object
 mailItem = olApp.CreateItem(0)
-mailItem.Subject = ((target_name[:-4]) + " " + customer)
+mailItem.Subject = ((target_name[:-4]) + " " + nameToCustomer)
 mailItem.BodyFormat = 1
 mailItem.Body = "Hello World"
 mailItem.To = (mailForCustomer)
